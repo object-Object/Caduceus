12e1b4e42bbd434f649b0a8ed17d112fec222007
object-Object <object@objectobject.ca>
2025-06-16 16:32:44 -0400

    Remove example pattern

--- common/src/main/kotlin/gay/object/caduceus/registry/CaduceusActions.kt
+++ common/src/main/kotlin/gay/object/caduceus/registry/CaduceusActions.kt
@@ -6,14 +6,11 @@ import at.petrak.hexcasting.api.casting.math.HexDir
 import at.petrak.hexcasting.api.casting.math.HexPattern
 import at.petrak.hexcasting.common.lib.HexRegistries
 import at.petrak.hexcasting.common.lib.hex.HexActions
-import gay.`object`.caduceus.casting.actions.spells.OpCongratulate
 
 object CaduceusActions : CaduceusRegistrar<ActionRegistryEntry>(
     HexRegistries.ACTION,
     { HexActions.REGISTRY },
 ) {
-    val CONGRATULATE = make("congratulate", HexDir.WEST, "eed", OpCongratulate)
-
     private fun make(name: String, startDir: HexDir, signature: String, action: Action) =
         make(name, startDir, signature) { action }
 
